0646d004b3dba7ee899ca3705e037c154eea987b
Max Clifford <me@mclifford.dev>
2022-09-10 13:01:32 +0100

    Added unversioned files

---
+++ Articulus/Import/Metadata.fs
@@ -0,0 +1,63 @@
+﻿namespace Articulus.Import
+
+module Metadata =
+
+    open System
+
+    [<RequireQualifiedAccess>]
+    type FetchKeyResult<'T> =
+        | Found of 'T
+        | NotFound
+        | ParsingError
+
+        member fkr.ToErrorMessage(key: string) =
+            match fkr with
+            | Found _ -> None
+            | NotFound -> Some $"Key `{key}` not found."
+            | ParsingError -> Some $"Key `{key}` could not be parsed."
+
+    type CommonMetadata =
+        { Title: string
+          Date: DateTime
+          Order: int }
+
+    let titleKey = "articulus:title"
+
+    let dateKey = "articulus:created_on"
+
+    let orderKey = "articulus:order"
+
+    let getTitle (md: Map<string, string>) =
+        md.TryFind titleKey
+        |> Option.map FetchKeyResult.Found
+        |> Option.defaultValue FetchKeyResult.NotFound
+
+    let getDate (md: Map<string, string>) =
+        match md.TryFind dateKey with
+        | Some dv ->
+            match DateTime.TryParse dv with
+            | true, v -> FetchKeyResult.Found v
+            | false, _ -> FetchKeyResult.ParsingError
+        | None -> FetchKeyResult.NotFound
+
+    let getOrder (md: Map<string, string>) =
+        match md.TryFind orderKey with
+        | Some ov ->
+            match Int32.TryParse ov with
+            | true, v -> FetchKeyResult.Found v
+            | false, _ -> FetchKeyResult.ParsingError
+        | None -> FetchKeyResult.NotFound
+
+    let getCommonMetadata (md: Map<string, string>) =
+        match getTitle md, getDate md, getOrder md with
+        | FetchKeyResult.Found title, FetchKeyResult.Found date, FetchKeyResult.Found order ->
+            { Title = title
+              Date = date
+              Order = order }
+            |> Ok
+        | title, date, order ->
+            [ title.ToErrorMessage(titleKey)
+              date.ToErrorMessage(dateKey)
+              order.ToErrorMessage(orderKey) ]
+            |> List.choose id
+            |> Error
---
+++ Articulus/Rendering/Impl.fs
@@ -0,0 +1,118 @@
+﻿namespace Articulus.Rendering
+
+open System
+open System.IO
+open System.Text
+open System.Text.Json.Serialization
+open Articulus.Store
+open FDOM.Core.Common
+open FDOM.Rendering
+open Fluff.Core
+
+[<AutoOpen>]
+module Impl =
+
+    [<CLIMutable>]
+    type FragmentData =
+        { [<JsonPropertyName("title")>]
+          Title: string
+          [<JsonPropertyName("items")>]
+          Items: FragmentDataItem seq }
+
+    and [<CLIMutable>] FragmentDataItem =
+        { [<JsonPropertyName("titleHtml")>]
+          TitleHtml: string
+          [<JsonPropertyName("summaryHtml")>]
+          SummaryHtml: string
+          [<JsonPropertyName("link")>]
+          Link: string
+          [<JsonPropertyName("date")>]
+          Date: DateTime }
+
+    let createBlocks (lines: string list) =
+        FDOM
+            .Core
+            .Parsing
+            .Parser
+            .ParseLines(lines)
+            .CreateBlockContent()
+
+    let getContent (block: DOM.BlockContent) =
+        match block with
+        | DOM.Header headerBlock -> Some headerBlock.Content
+        | DOM.Paragraph paragraphBlock -> Some paragraphBlock.Content
+        | _ -> None
+
+    let getFragmentDataItem (link: string) (blocks: DOM.BlockContent list) =
+        // Take the first header and paragraph.
+        match blocks.Length > 2 with
+        | true ->
+            match getContent blocks[0], getContent blocks[1] with
+            | Some titleContent, Some summaryContent ->
+                { TitleHtml = Html.renderInlineItems titleContent
+                  SummaryHtml = Html.renderInlineItems summaryContent
+                  Link = link
+                  Date = DateTime.Now }
+                |> Ok
+            | None, _ -> Error "Could not get title content."
+            | _, None -> Error "Could not get summary content"
+        | false -> Error ""
+
+    let renderArticle
+        (data: Mustache.Data)
+        (template: Mustache.Template)
+        (outputPath: string)
+        (name: string)
+        (blocks: DOM.BlockContent list)
+        =
+
+        try
+            Html.renderBlocksWithTemplate template data blocks
+            |> fun r -> File.WriteAllText(Path.Combine(outputPath, $"{name}.html"), r)
+
+            Ok blocks
+        with
+        | exn -> Error $"Unhandled exception while rendering article `{name}`. Message: {exn.Message}"
+
+    let render
+        (store: ArticulusStore)
+        (values: (string * Mustache.Value) list)
+        (template: Mustache.Template)
+        (outputRoot: string)
+        (outputDirectory: string)
+        (name: string)
+        =
+        match store.GetLatestArticleVersion(name, false) with
+        | Some avr ->
+            let outputPath =
+                Path.Combine(outputRoot, outputDirectory)
+
+            if Directory.Exists outputPath |> not then
+                Directory.CreateDirectory outputPath |> ignore
+
+            let data =
+                ({ Values = values @ [] |> Map.ofList
+                   Partials = Map.empty }: Mustache.Data)
+
+            let link =
+                $"./{outputDirectory}/{name}.html"
+
+            avr.RawBlob.ToBytes()
+            |> Encoding.UTF8.GetString
+            |> fun r -> r.Split Environment.NewLine
+            |> List.ofArray
+            |> FDOM.Core.Parsing.Parser.ParseLines
+            |> fun p -> p.CreateBlockContent()
+            |> renderArticle data template outputPath name
+            |> Result.bind (getFragmentDataItem link)
+        | None -> Error $"Article `{name}` not found."
+
+    let renderAll
+        (store: ArticulusStore)
+        (values: (string * Mustache.Value) list)
+        (template: Mustache.Template)
+        (outputRoot: string)
+        (outputDirectory: string)
+        =
+        store.GetAllArticles()
+        |> List.map (fun ar -> render store values template outputRoot outputDirectory ar.Name)
