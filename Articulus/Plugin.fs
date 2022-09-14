a916202887dc982dc33952dc0030401eaa4f2ba5
Max Clifford <me@mclifford.dev>
2022-09-14 22:15:39 +0100

    Got basic plugin working

---
+++ Articulus/Plugin.fs
@@ -0,0 +1,24 @@
+﻿namespace Articulus
+
+module Plugin =
+
+    open System.Text.Json.Serialization
+
+    [<CLIMutable>]
+    type ArticulusConfiguration =
+        { [<JsonPropertyName("storePath")>]
+          StorePath: string
+          [<JsonPropertyName("templatePath")>]
+          TemplatePath: string
+          [<JsonPropertyName("dataPath")>]
+          DataPath: string
+          [<JsonPropertyName("outputPath")>]
+          OutputPath: string
+          [<JsonPropertyName("outputDirectory")>]
+          OutputDirectory: string
+          [<JsonPropertyName("indexPageTemplatePath")>]
+          IndexPageTemplatePath: string
+          [<JsonPropertyName("indexPageName")>]
+          IndexPageName: string
+          [<JsonPropertyName("fragmentName")>]
+          FragmentName: string }
